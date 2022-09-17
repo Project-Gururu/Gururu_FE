4f80a3e1dc62d6bd581265002674c00e2128f63d
donghyunKang-dev <make_w@naver.com>
2022-09-18 01:14:04 +0900

    Style: tailwind css 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -6,6 +7,20 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: '#D7C0AE',
+      secondary: '#EEE3CB',
+      white: '#FFFFFF',
+      gray: '#f3f3f3',
+    },
+    fontFamily: {
+      sans: ['Black Han Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
