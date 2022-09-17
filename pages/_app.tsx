13be15f16c013851e1d3ca33448c3e6b2daa78e6
donghyunKang-dev <make_w@naver.com>
2022-09-17 19:03:08 +0900

    Feat: tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import type { AppProps } from 'next/app'
-
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from 'styles/globalstyles'
 import theme from 'styles/theme'
+import 'styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
