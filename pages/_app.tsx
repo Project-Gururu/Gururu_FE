1b2d7f016ea6e4fe141a50c7ba1f1436dce36f2c
donghyunKang-dev <make_w@naver.com>
2022-09-18 23:01:48 +0900

    Feat: scss setting-2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,5 @@ import type { AppProps } from 'next/app'
 import 'styles/global.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
