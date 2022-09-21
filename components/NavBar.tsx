d3efe9abd92d0de53dd6b77f10ac642daed7b6d2
donghyunKang-dev <make_w@naver.com>
2022-09-22 00:00:20 +0900

    Feat: navigation bar 생성

---
+++ components/NavBar.tsx
@@ -0,0 +1,29 @@
+import styles from 'styles/components/NavBar.module.scss'
+
+import SearchIcon from 'public/images/search.svg'
+import LiveIcon from 'public/images/live.svg'
+import MyIcon from 'public/images/user.svg'
+import ReservationIcon from 'public/images/reservation.svg'
+
+export default function NavBar() {
+  return (
+    <nav className={styles.container}>
+      <div className={styles.wrap}>
+        <SearchIcon alt="" width="22" height="22" fill="gray" />
+        검색
+      </div>
+      <div className={styles.wrap}>
+        <LiveIcon alt="" width="22" height="22" fill="gray" />
+        LIVE
+      </div>
+      <div className={styles.wrap}>
+        <ReservationIcon alt="" width="22" height="22" fill="gray" />
+        예약현황
+      </div>
+      <div className={styles.wrap}>
+        <MyIcon alt="" width="22" height="22" fill="gray" />
+        마이페이지
+      </div>
+    </nav>
+  )
+}
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,11 +1,12 @@
-import styles from 'styles/pages/home.module.scss'
-import React from 'react'
+import NavBar from 'components/NavBar'
 import KakaoMap from 'components/KakaoMap'
+import styles from 'styles/pages/home.module.scss'
 
 export default function Map() {
   return (
     <div className={styles.container}>
       <KakaoMap />
+      <NavBar />
     </div>
   )
 }
