6a79284029d5c5688db7af6fc21103374fa327a4
donghyunKang-dev <make_w@naver.com>
2022-09-26 23:47:20 +0900

    Feat: 카카오 로그인 취소 처리

--- pages/user/kakao/callback.tsx
+++ pages/user/kakao/callback.tsx
@@ -4,20 +4,25 @@ import axios from 'axios'
 
 const Kakao = () => {
   const router = useRouter()
-  const { code } = router.query
+  const { code, error: cancel } = router.query
 
   React.useEffect(() => {
+    if (cancel) router.push('/')
     if (code === undefined) return
+
     const fetchData = async () => {
       try {
-        const response = await axios.post(
+        await axios.post(
           `${process.env.NEXT_PUBLIC_API_URL}/auth/kakao?code=${code}`,
         )
         router.push('/map')
-      } catch (error) {}
+      } catch (error) {
+        throw new Error(`에러발생`)
+      }
     }
+
     fetchData()
-  }, [code, router])
+  }, [code, cancel, router])
 
   return <div>잠시만 기다려주세요!</div>
 }
