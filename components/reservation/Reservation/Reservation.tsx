be1475f4681daf9b97c3f20fc6ee7916d63f27d2
donghyunKang-dev <make_w@naver.com>
2022-10-26 16:13:53 +0900

    Feat: 예약 내역 페이지 api 연결

--- components/reservation/Reservation/Reservation.tsx
+++ components/reservation/Reservation/Reservation.tsx
@@ -1,58 +1,42 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import ReservationList from '../ReservationList/ReservationList'
 
-import ArrowIcon from 'public/images/arrow-right.svg'
 import s from './Reservation.module.scss'
-import Button from 'components/ui/Button/Button'
 
 export default function Reservation() {
-  const [focusBtn, setFocusBtn] = React.useState<number>(0)
-  const status = React.useMemo(() => ['대기중', '진행중', '완료', '거절'], [])
+  const [focusStatus, setFocusStatus] = React.useState<string>('waiting')
+  const status = React.useMemo<{ [key: string]: string }>(() => {
+    return {
+      waiting: '대기 중',
+      progress: '진행 중',
+      completion: '완료',
+      refuse: '거절',
+    }
+  }, [])
 
   return (
     <section className={s.container}>
       <header className={s.tab}>
-        {status.map((value, index) => {
+        {Object.keys(status).map((value, index) => {
           return (
             <button
               key={index}
-              className={focusBtn === index ? s.focus : ''}
-              onClick={() => setFocusBtn(index)}
+              className={focusStatus === value ? s.focus : ''}
+              onClick={() => setFocusStatus(value)}
             >
               <span>0</span>
-              {value}
+              {status[value]}
             </button>
           )
         })}
       </header>
       <article>
-        <ul>
-          <li className={s.itemList}>
-            <div className={s.itemContainer}>
-              <div className={s.itemDate}>
-                2011.02.09
-                <ArrowIcon
-                  width={35}
-                  height={35}
-                  alt="예약내역 상세보기"
-                  stroke="black"
-                />
-              </div>
-              <div className={s.itemWrap}>
-                <div className={s.itemLeft}>
-                  <p>킴스애견</p>
-                  <p>컷트 -소형</p>
-                  <p>100,000원</p>
-                </div>
-                <div className={s.itemRight}>
-                  <div>강아지사진</div>
-                </div>
-              </div>
-              <Button variant="naked" width="100%">
-                후기 작성
-              </Button>
-            </div>
-          </li>
-        </ul>
+        <Suspense fallback={<div>스켈레톤</div>}>
+          <ReservationList
+            statusEn={focusStatus}
+            statusKo={status[focusStatus]}
+          />
+        </Suspense>
       </article>
     </section>
   )
